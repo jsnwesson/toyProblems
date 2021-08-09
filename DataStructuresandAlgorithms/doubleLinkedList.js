f263113cdd17e89b4c94333cc5f36e09af6884cc
Jason Wesson <jsnwesson@gmail.com>
2021-08-09 15:55:27 -0500

    create get() and unshift() for doubleLinkedList  Please enter the commit message for your changes. Lines starting

--- DataStructuresandAlgorithms/doubleLinkedList.js
+++ DataStructuresandAlgorithms/doubleLinkedList.js
@@ -41,11 +41,73 @@ class DoublyLinkedList{
     this.length--;
     return removed;
   }
+  shift(){
+    if (!this.head) return null;
+    let removed = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = removed.next;
+      this.head.prev = null;
+    }
+    removed.next = null;
+    this.length--;
+    return removed;
+  }
+  unshift(val){
+    let newHead = new Node(val);
+    if (!this.head) {
+      this.head = newHead;
+      this.tail = this.head;
+    } else {
+      this.head.prev = newHead;
+      newHead.next = this.head;
+      this.head = newHead;
+    }
+
+    this.length++;
+    return this;
+  }
+  get(index){
+    if (index < 0 || index >= this.length) return null;
+
+    let mid = Math.floor(this.length / 2);
+    let counter;
+    let node = this.head;
+
+
+    if (index <= mid) {
+      counter = 0;
+      while (counter !== index) {
+        node = node.next;
+        counter++;
+      }
+    } else {
+      counter = this.length - 1;
+      node = this.tail;
+      while(counter !== index){
+        console.log(node)
+        node = node.prev;
+        counter--;
+      }
+    }
+    return node;
+  }
+  set(index, value){
+
+  }
+  insert(index, value) {
+
+  }
+  remove(index){
+
+  }
 }
 
 let list = new DoublyLinkedList;
 list.push(100);
 list.push(10);
 list.push('last item');
-list.pop();
-console.log(list)
+list.unshift(1000);
+console.log(list.get(3))
