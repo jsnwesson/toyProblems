ca3803cfdc7d904d8d4d102c1a09b6499f8ad996
Jason Wesson <jsnwesson@gmail.com>
2021-08-09 21:06:24 -0500

    instantiate a Queue

---
+++ DataStructuresandAlgorithms/queue.js
@@ -0,0 +1,19 @@
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+class Queue{
+  constructor(){
+    this.start = null;
+    this.last = null;
+    this.size = 0;
+  }
+  enqueue(value){
+
+  }
+  dequeue(){
+
+  }
+}
