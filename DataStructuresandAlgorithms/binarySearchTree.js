787ba4f959a221e6d2d58f92f6d7b79e120d3269
Jason Wesson <jsnwesson@gmail.com>
2021-08-10 19:11:49 -0500

    attempt to implement insert(value) into BST

--- DataStructuresandAlgorithms/binarySearchTree.js
+++ DataStructuresandAlgorithms/binarySearchTree.js
@@ -10,7 +10,32 @@ class BinarySearchTree {
   constructor(){
     this.root = null;
   }
+  insert(value){
+    console.log(this);
+    let newNode = new Node(value)
+    if (!this.root) {
+      this.root = newNode;
+    }
+    if (newNode.value < this.root.value) {
+      if (this.root.left) {
+        this.root.left.insert(value)
+      } else {
+        this.root.left = newNode;
+      }
+    } else if (newNode.value > this.root.value) {
+      if (this.root.right) {
+        this.root.right.insert(value);
+      } else {
+        this.root.right = newNode;
+      }
+    }
+    return this;
+  }
 }
 
 let tree = new BinarySearchTree();
-console.log(tree.root)
+tree.insert(10);
+tree.insert(8);
+tree.insert(11);
+tree.insert(7);
+console.log('this is the tree:', tree)
